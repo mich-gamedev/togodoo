ba6a9810baeaef6eac712f8585e8d78aa792322d
_michdev <mich.gamedev@gmail.com>
2025-04-03 10:40:26 -0400

    added Separator block

---
+++ blocks/items/separator/separator.gd
@@ -0,0 +1,17 @@
+extends HBoxContainer
+
+@onready var block: Block = $Block
+@onready var title: RichTextLabel = $Title
+@onready var left: HSeparator = $Left
+@onready var right: HSeparator = $Right
+
+func _update_block() -> void:
+	title.text = block.get_title()
+	left.size_flags_stretch_ratio = block.get_arg("left_ratio")
+	right.size_flags_stretch_ratio = block.get_arg("right_ratio")
+	if !(block.get_title() and block.get_arg("show_title")):
+		title.hide()
+		right.hide()
+	else:
+		title.show()
+		right.show()

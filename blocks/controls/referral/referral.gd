c673a2170e0d46359008a94ac9a67b70ad47c598
_michdev <mich.gamedev@gmail.com>
2025-04-03 09:56:34 -0400

    made scrollbars fade out over time, added Referral block

--- blocks/controls/referral/referral.gd
+++ blocks/controls/referral/referral.gd
@@ -3,8 +3,12 @@ extends Button
 @onready var block: Block = $Block
 
 func _update_block() -> void:
-	icon = ImageTexture.create_from_image(Image.load_from_file(block.get_arg("icon")))
+	if ResourceLoader.exists(block.get_arg("icon")):
+		icon = ImageTexture.create_from_image(Image.load_from_file(block.get_arg("icon")))
+	else:
+		icon = null
 	text = block.get_title()
+	disabled = !FileAccess.file_exists(block.get_arg("project"))
 
 func _on_pressed() -> void:
 	OS.create_process(OS.get_executable_path(), ["--project=%s" % block.get_arg("project")])

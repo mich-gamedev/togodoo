40d5a66c9ad414c531bf091469cdc08cd1107d39
_michdev <mich.gamedev@gmail.com>
2025-04-04 13:40:29 -0400

    added sizing options to Image block

--- blocks/items/image/image.gd
+++ blocks/items/image/image.gd
@@ -5,3 +5,9 @@ extends TextureRect
 func _update_block() -> void:
 	texture = ImageTexture.create_from_image(Image.load_from_file(block.get_arg("image_path")))
 	tooltip_text = block.get_title()
+	if block.get_arg("width") <= 0:
+		size_flags_horizontal = SIZE_EXPAND_FILL
+		custom_minimum_size.x = 0
+	else:
+		size_flags_horizontal = SIZE_SHRINK_BEGIN
+		custom_minimum_size.x = block.get_arg("width")

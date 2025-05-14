f6ac3606c707f4d69a582c3084d71a6c35479523
_michdev <150397626+mich-gamedev@users.noreply.github.com>
2025-05-14 13:37:57 -0400

    added documentation to commands

--- components/cmd_pallete/command.gd
+++ components/cmd_pallete/command.gd
@@ -1,7 +1,8 @@
 class_name Command
 
-var name: String
-var function: Callable
-var is_valid: Callable
-var inputs: Array[InputEvent]
-var icon: Texture2D
+var name: String ## the displayed name in the command prompt list
+var function: Callable ## the function that gets called when the command is picked
+var is_valid: Callable ## the function to check if the command is valid and can be ran
+var inputs: Array[InputEvent] ## shortcut events to activate commands
+var icon: Texture2D ## the displayed icon in the command prompt list
+var sub_options: Array[Variant] ## if not empty, will be used as a dropdown and provided as an argument to [member function]

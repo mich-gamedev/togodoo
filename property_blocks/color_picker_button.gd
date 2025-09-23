f4001abcbbac213b148a900d9b7746abeb6e43b5
_michdev <gamedev.mich@gmail.com>
2025-09-23 10:02:31 -0400

    updated property_color usage tag to allow for html code strings

--- property_blocks/color_picker_button.gd
+++ property_blocks/color_picker_button.gd
@@ -10,3 +10,12 @@ func _ready() -> void:
 
 func _on_picker_created() -> void:
 	get_popup().hide()
+
+func _update(value: Variant):
+	match typeof(value):
+		TYPE_COLOR:
+			color = value
+		TYPE_STRING:
+			var string := String(value)
+			if string.is_valid_html_color():
+				color = Color(string)

44cb5cc2f79b3c224de0c812e801ba4e45135f75
_michdev <gamedev.mich@gmail.com>
2025-10-16 11:08:07 -0400

    fixed favorite block list emptying when settings are changed

--- scenes/main_refactor/favorite_blocks.gd
+++ scenes/main_refactor/favorite_blocks.gd
@@ -7,16 +7,16 @@ func _ready() -> void:
 	Settings.signals.setting_changed.connect(update)
 
 func update(mod: String, setting: String, value: Variant) -> void:
+	if [mod, setting] != ["vanilla", "editor/favorite_blocks"]: return
 	for i in get_children():
 		i.queue_free()
-	if [mod, setting] == ["vanilla", "editor/favorite_blocks"]:
-		for i in value:
-			var inst : Button = preload("uid://bfellyvhea3i3").instantiate()
-			var cfg = FileManager.get_block_config_by_type(i)
-			inst.icon = load(cfg.get_value("display", "icon"))
-			inst.block_type = i
-			add_child(inst)
-			inst.pressed.connect(_pressed.bind(i))
+	for i in value:
+		var inst : Button = preload("uid://bfellyvhea3i3").instantiate()
+		var cfg = FileManager.get_block_config_by_type(i)
+		inst.icon = load(cfg.get_value("display", "icon"))
+		inst.block_type = i
+		add_child(inst)
+		inst.pressed.connect(_pressed.bind(i))
 
 func _pressed(type: StringName) -> void:
 	TreeManager.create_default_block(type, TreeManager.get_valid_parent(tree.tree_items.find_key(tree.get_selected())) if tree.get_selected() else TreeManager.get_root())
